8e38e6277ca0d78cca6dc97743a52f693041ceb6
FuBuKi <tiendat204.tranvan@gmail.com>
2025-06-07 16:05:13 +0700

    create app

---
+++ navigation/Navigator.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class Navigator extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Navigator</Text>
+      </View>
+    )
+  }
+}
